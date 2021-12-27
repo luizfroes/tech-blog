4f0932701165bcd09e5a9a45204ebf43f55d27f2
Luiz Carlos Froes <luizfroes@gmail.com>
2021-12-27 22:02:13 +0000

    Added comments model

--- src/models/Comments.js
+++ src/models/Comments.js
@@ -1,10 +1,47 @@
 // import important parts of sequelize library
-const { Model, DataTypes, ConnectionError } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 
 // import our database connection from config.js
 const connection = require("../config/connection");
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  comment: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "User",
+      key: "id",
+    },
+  },
+  blog_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "Blog",
+      key: "id",
+    },
+  },
+};
+
+const options = {
+  sequelize: connection,
+  timestamps: true,
+  freezeTableNames: true,
+  underscored: true,
+  modelName: "comments",
+};
+
+// Initialize Comments model (table) by extending off Sequelize's Model class
 class Comments extends Model {}
+
+Comments.init(schema, options);
 
 module.exports = Comments;
