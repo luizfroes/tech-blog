85d58df3dd9c4e9081759386dcfb7d9812d6b229
Luiz Carlos Froes <luizfroes@gmail.com>
2021-12-27 22:01:56 +0000

    added blog model

--- src/models/Blog.js
+++ src/models/Blog.js
@@ -1,10 +1,44 @@
 // import important parts of sequelize library
-const { Model, DataTypes, ConnectionError } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 
 // import our database connection from config.js
 const connection = require("../config/connection");
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "User",
+      key: "id",
+    },
+  },
+};
+
+const options = {
+  sequelize: connection,
+  timestamps: true,
+  freezeTableNames: true,
+  underscored: true,
+  modelName: "blog",
+};
+
+// Initialize Blog model (table) by extending off Sequelize's Model class
 class Blog extends Model {}
+
+Blog.init(schema, options);
 
 module.exports = Blog;
