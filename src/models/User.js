a3ad744b5d8fe8d17dc60a275231545598baa831
Luiz Carlos Froes <luizfroes@gmail.com>
2021-12-27 20:25:16 +0000

    Added user model

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,60 @@ const { Model, DataTypes, ConnectionError } = require("sequelize");
 // import our database connection from config.js
 const connection = require("../config/connection");
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+// Initialize User model (table) by extending off Sequelize's Model class
 class User extends Model {}
+
+const hashPassword = require("../../hooks/hashPassword");
+
+const schema = {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [8, 20],
+    },
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+};
+
+const options = {
+  sequelize: connection,
+  timestamps: true,
+  freezeTableNames: true,
+  underscored: true,
+  modelName: "user",
+  hooks: {
+    beforeCreate: hashPassword,
+  },
+};
+
+class User extends Model {
+  async checkPassword(userPassword) {
+    const isValid = await bcrypt.compare(userPassword, this.password);
+    return isValid;
+  }
+}
+
+User.init(schema, options);
 
 module.exports = User;
