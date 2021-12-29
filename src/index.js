0fd3b0b39cdea96eee776934641c7bb52659171a
Luiz Carlos Froes <luizfroes@gmail.com>
2021-12-29 00:59:17 +0000

    added handlebars files

--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ const { logInfo, logError } = require("./helpers/logger");
 const path = require("path");
 
 const express = require("express");
+const expressHandlebars = require("express-handlebars");
 
 const routes = require("./routes");
 const connection = require("../config/connection");
 
 const PORT = process.env.PORT || 3000;
 
+const hbs = expressHandlebars.create({});
 const app = express();
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
